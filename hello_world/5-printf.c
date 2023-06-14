c03d55500d31e72940285645f09b95fe9b676caa
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-14 10:51:33 -0700

    Exercise 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - main function to execute the file
+ * Return: return a specific string
+ */
+int main(void)
+{
+	/**
+	 * Strings are a sequence of characters terminated with a null character \0
+	 * for example, 'Hola ' => [[H][o][l][a][ ][\0]]
+	 */
+	char str[] = "with proper grammar, but the outcome is a piece of art,"; 
+	printf("%s\n", string);
+	return (0);
+}
