1ce4034775ffa8980419a0c6dce50505b3a08281
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-14 10:57:35 -0700

    Exercise 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -9,7 +9,9 @@ int main(void)
 	 * Strings are a sequence of characters terminated with a null character \0
 	 * for example, 'Hola ' => [[H][o][l][a][ ][\0]]
 	 */
-	char str[] = "with proper grammar, but the outcome is a piece of art,"; 
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
 	printf("%s\n", str);
+
 	return (0);
 }
