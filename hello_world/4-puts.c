f4f8c08990a463390c998dd70172bd1ed2bdec69
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-14 10:05:09 -0700

    Exercise 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-int main(void) {
+int main(void)
+{
 	/**
 	 * Strings are a sequence of characters terminated with a null character \0
 	 * for example, 'Hola ' => [[H][o][l][a][ ][\0]]
