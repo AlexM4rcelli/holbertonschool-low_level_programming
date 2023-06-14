0801327564903ca0e211be9c1e171c575d5ff34d
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-14 09:55:52 -0700

    Exercise: 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-int main() {
-  /* Strings are a sequence of characters terminated with a null character \0 
-   *
-   * for example, 'Hola ' => [[H][o][l][a][ ][\0]] 
-   * */	
-  char str[] = "Programming is like building a multilingual puzzle";
-  puts(str);
-  return 0;
+int main(void) {
+	/** 
+	 * Strings are a sequence of characters terminated with a null character \0
+	 * for example, 'Hola ' => [[H][o][l][a][ ][\0]] 
+	 */	
+	puts("\"Programming is like building a multilingual puzzle\"");
+	
+	return 0;
 }
