4bde6edd06fd274c17b60dafb600aad9588784ac
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-15 05:56:00 -0700

    Exercise 9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 /**
  * main - main function to execute the file
  * Return: return a specific string
@@ -7,9 +7,12 @@
 int main(void)
 {
 	/**
-	 * write() envía bytes (que normalmente representan caracteres, pero no necesariamente) hacia un flujo de salida (el primer parámetro), que suele ser un fichero previamente abierto con open() (aunque no en tu caso). El segundo parámetro es la dirección donde están los bytes a enviar y el tercero es el número de bytes a enviar. En cambio en tu caso el flujo de salida es 1 que representa la salida estándar (es decir, la terminal), la dirección es la de la variable c, y la cantidada envia
+	 * write() envía bytes (que normalmente representan caracteres, pero no necesariamente) hacia un flujo de salida (el primer parámetro), que suele ser un
+	 * fichero previamente abierto con open() (aunque no en tu caso). El segundo parámetro es la dirección donde están los bytes a enviar y el tercero es el
+	 * número de bytes a enviar. En cambio en tu caso el flujo de salida es 1 que representa la salida estándar (es decir, la terminal), la dirección es la
+	 * de la variable str, y la cantidada enviada la longitud de str.
 	 */
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(1,%str, strlen(str))
+	write(1, &str, strlen(str));
 	return (1);
 }
