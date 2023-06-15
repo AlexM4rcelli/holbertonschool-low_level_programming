0c7e2c4b94b7750f78c5e10bc2c20b30a8f7f21c
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-15 09:26:56 -0700

    Exercise 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function to execute the file
+ * Return: if the input is positive,negative or zero
+ */
+int main(void)
+{
+	int letter = 97;
+
+	for (letter ; letter <= 122; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar(10);
+
+}
