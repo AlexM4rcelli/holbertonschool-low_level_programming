10fd7ad6d715f212be6f263ebbe9acccd2b88954
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-15 07:01:46 -0700

    Exercise 0

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,17 +10,11 @@ int main(void)
 	 * for example, 'Hola ' => [[H][o][l][a][ ][\0]]
 	 */
 
-	char charType;
-	int intType;
-	long int longIntType;
-	long long int longlongType;
-	float floatType;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
