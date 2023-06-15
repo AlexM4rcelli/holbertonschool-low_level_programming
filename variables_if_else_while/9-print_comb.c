80428ac0df55ac2851360fba2b9d9f2a6b3b2aae
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-15 10:21:26 -0700

    Exercise 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function to execute the file
+ * Return: if the input is positive,negative or zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (num < 57)
+			putchar(',');
+			putchar(' ');
+	}
+
+	putchar(10);
+
+	return (0);
+}
