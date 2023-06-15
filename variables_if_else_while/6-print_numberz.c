c302e11a481098f00e79d9cbf333ab05715855bc
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-15 09:52:20 -0700

    Exercise 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function to execute the file
+ * Return: if the input is positive,negative or zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+
+	putchar(10);
+
+	return (0);
+}
