1d3eda13178f5f62cad8b6665dcf4372f212a4e3
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-17 05:40:39 -0700

    Exercise 11

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -16,16 +16,18 @@ int main(void)
 	for (num = 48; num < 57; num++)
 	{
 		int i;
+
 		for (i = num + 1; i < 57; i++)
 		{
 			int j = i + 1;
-			while ( j <= 57)
+
+			while (j <= 57)
 			{
 				putchar(num);
 				putchar(i);
 				putchar(j);
 				j++;
-				
+
 				if (num < 55)
 				{
 					putchar(',');
