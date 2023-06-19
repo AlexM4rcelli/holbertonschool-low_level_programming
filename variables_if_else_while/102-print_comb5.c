d9a8adf52a12182bf443a2ff7038aee5db07b15e
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-19 06:03:47 -0700

    Exercise 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function to execute the file
+ * Return: if the input is positive,negative or zero
+ */
+int main(void)
+{
+	int j = 0;
+	int b;
+
+	for (j = 0; j <= 98; j++)
+	{ 
+		b = j + 1;
+	while (b != 100)
+	{
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		b++;
+	
+		if(j != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	}
+	putchar(10);
+
+	return (0);
+}
