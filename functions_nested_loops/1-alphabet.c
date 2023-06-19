3671ae70ad30e0e4804d2e26291234b344c27cfe
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-19 07:53:45 -0700

    Exercise 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+ #include <stdio.h>
+
+ /*
+  * main - Execute the program
+  * Return - something
+  */
+void
+print_alphabet(void)
+{
+
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(10);
+
+}
