0d4fc6f169119f4fbacb9ee5e7c2bfbb59af12e3
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-19 08:00:07 -0700

    exercise 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+ #include <stdio.h>
+
+ /*
+  * main - Execute the program
+  * Return - something
+  */
+void
+print_alphabet_x10(void)
+{
+	int j;
+	for(j = 1; j <= 10; j++)
+	{
+		int i;
+
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar(10);
+	}
+}
