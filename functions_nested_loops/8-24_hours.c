4eb959f0efc89befbe3617eaf684feb34ed6d25f
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-19 09:03:53 -0700

    Exercise 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * main - main function to execute the file
+ * Return: if the input is positive,negative or zero
+ */
+void
+jack_bauer(void)
+{
+	int j = 0;
+	int b;
+
+	for (j = 0; j < 24; j++)
+	{
+		b = j;
+		while (b != 60)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+
+	}
+
+}
