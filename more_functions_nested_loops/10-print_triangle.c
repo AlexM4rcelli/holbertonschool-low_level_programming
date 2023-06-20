8cbc71f23457d798da21bae29e2d947de3de31eb
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-20 05:15:39 -0700

    Exercise 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+void
+print_triangle(int n)
+{
+	int i, j, k;
+
+	for (j = 0; j < n; j++)
+	{
+		if (j != 0)
+		{
+			for (k = n - 1; k > j; k--)
+			{
+				_putchar(' ');
+			}
+		}
+
+		for (i = 0; i < j; i++)
+		{
+
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
