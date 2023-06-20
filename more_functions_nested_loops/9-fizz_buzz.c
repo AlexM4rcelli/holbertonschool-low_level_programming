73d5a14ef9ec9ec1218beb762aeb7d067c1d8e0e
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-19 19:32:20 -0700

    exercise 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void
+main (void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("%s ","FizzBuzz");
+		else if (i % 5 == 0)
+			printf("%s ", "Buzz");
+		else if (i % 3 == 0)
+			printf("%s ", "Fizz");
+		else
+			printf("%d ", i);
+	}
+
+	putchar('\n');
+}
