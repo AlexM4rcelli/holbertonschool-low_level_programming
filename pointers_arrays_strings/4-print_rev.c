dd55b3fed81091865a0caefda211bbf359eea6e5
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-22 05:43:22 -0700

    #4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_rev - print a string
+ *@str: the string to print
+ */
+
+void
+print_rev(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	for (j = i; j >= 0; j--)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
