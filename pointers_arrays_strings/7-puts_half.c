4b88f41192461b1388596482647cfab05ec117bd
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-22 08:18:49 -0700

    #7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_puts2 - print a string
+ *@str: the string to print
+ */
+
+void
+puts_half(char *str)
+{
+	int i, j;
+
+	for (j = 0; str[j] != '\0'; j++)
+		;
+
+	i = j / 2;
+
+	while (i <= j)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
