4bd932e05355faf2ac44b909e0f31c319c75a83d
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-24 10:01:23 -0700

    #2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char
+*_strncpy(char *dest, char *src, int n)
+{
+	int len2;
+
+	for (len2 = 0; src[len2] != '\0'; len2++)
+	{
+		if (len2 < n)
+		{
+			dest[len2] = src[len2];
+		}
+	}
+
+	dest[len2] = '\0';
+
+	return (dest);
+}
