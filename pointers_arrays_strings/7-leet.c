56b6cef48d5875e5369f5ef79b3ce686e59ab526
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-25 19:28:05 -0700

    #7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+char
+*leet(char *c)
+{
+	int num[5] = {4, 3, 0, 7, 1};
+
+	char lett[] = "aeotl";
+
+	int len1, len2;
+
+	for (len1 = 0; c[len1] != '\0'; len1++)
+	{
+		for (len2 = 0; len2 < 4; len2++)
+		{
+			if (c[len1] == lett[len2] || c[len1] == (lett[len2] - 32))
+			{
+				c[len1] = num[len2] + 48;
+			}
+		}
+
+		len2 = 0;
+	}
+
+	return (c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 int _putchar(char c);
-char *string_toupper(char *);
+char *leet(char *);
+char *string_toupper(char c);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
