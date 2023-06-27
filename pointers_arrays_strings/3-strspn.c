a1a41bd919054ca82f7bc4ecf5be38be23579340
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-27 08:37:45 -0700

    #3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+unsigned int
+_strspn(char *s, char *accept)
+{
+	int len, lens, lena;
+
+	len = 0;
+
+	for (lens = 0; s[lens] != '\0'; lens++)
+	{
+		for (lena = 0; accept[lena] != '\0'; lena++)
+		{
+			if (s[lens] == accept[lena])
+			{
+				len++;
+			}
+		}
+	}
+
+	return (len);
+}
