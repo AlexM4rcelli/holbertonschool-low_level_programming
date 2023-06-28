042af9f4d27a01c691d758ec0d038bc0b7e29671
AlexM4rcelli <6780@holbertonstudents.com>
2023-06-28 07:47:52 -0700

    #1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Write a string charcter by charcter in reverse
+ * @s: String to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
