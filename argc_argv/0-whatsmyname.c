f184e0c9f90176d65bc93903783a7bf503bc7314
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-02 09:35:08 -0700

    #0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-/*
- *main - print the name of the file
+/**
+ * main - Entry point
+ * @argc: number of parameters
+ * @argv: An array with the parameters passed to the function
+ * Description: This function prints the name of its function
+ * Return: Aways zero
  */
 int main(int argc, char *argv[])
 {
