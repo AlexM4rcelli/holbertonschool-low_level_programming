3a6fe8c4e6e1f0346b0bf01edb3e2554bdaa555f
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-05 10:37:11 -0700

    #1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+# include "main.h"
+# include <stdlib.h>
+
+int getLen(char *s);
+/**
+ * string_nconcat - Creates space in memory for a new concated array
+ * @s1: array to concat
+ * @s2: array to concat
+ * @n: num of bytes to concat
+ * Return: pointer to the space allocated
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len1, len2, j, totalLen;
+	char *array;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = getLen(s1);
+	len2 = getLen(s2);
+
+	if (n >= len2)
+		n = len2;
+
+	totalLen = len1 + len2 + 1;
+	array = malloc(totalLen);
+
+	if (!array)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	while (i < totalLen - 1 && j < n)
+	{
+		if (i < len1)
+		{
+			array[i] = s1[i];
+			i++;
+		}
+		else
+		{
+			array[i] = s2[j];
+			j++;
+			i++;
+		}
+	}
+
+	array[totalLen] = '\0';
+
+	return (array);
+}
+
+int
+getLen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
