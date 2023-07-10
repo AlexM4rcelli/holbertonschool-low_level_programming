54a7a17d98d0e073aaa790d12affe07e41b7ecc8
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-09 18:37:09 -0700

    #3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+# include <stdlib.h>
+int
+*array_range(int min, int max)
+{
+	int *array;
+	int l, i;
+
+	if (min > max)
+		return (NULL);
+
+	l = max - min;
+	i = 0;
+	array = malloc((l * sizeof(int)) + sizeof(int));
+
+	if (!array)
+		return (NULL);
+
+	while (i < l)
+	{
+		array[i] = min;
+		min++;
+		i++;
+	}
+	return (array);
+}
