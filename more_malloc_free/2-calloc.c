5e8fbbe6dc870d303169531b0afddfc1da9e8372
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-09 18:26:31 -0700

    #2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -4,18 +4,19 @@
 void
 *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
+	unsigned int i, l;
 	char *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(nmemb * size);
+	l = nmemb * size;
+	array = malloc(l);
 
 	if (!array)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < l; i++)
 		array[i] = 0;
 
 	return (array);
