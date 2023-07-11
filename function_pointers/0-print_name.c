8506cf542597a18cc012ece9e1fc89b1b712daf5
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-11 09:39:42 -0700

    #0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - print a name
+ * @name: name of the person
+ * @f: function what really print the name
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_
+#define _MAIN_
+# include <stdio.h>
+# include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
