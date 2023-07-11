8696e9454434d060e0cf06ada7ffca9c06412608
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-11 10:33:14 -0700

    #1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@ array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array && action)
 	{
-		long unsigned int i;
+		unsigned long int i;
 
 		for (i = 0; i < size; i++)
 			action(array[i]);
