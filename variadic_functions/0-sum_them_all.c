853d41bc556920c4ec9316f26b717baf635939fd
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-12 10:06:32 -0700

    #0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+# include <stdarg.h>
+
+/**
+ *
+ */
+
+int
+sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+
+	va_start(args, n);
+
+	int res = 0;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		res += x;
+	}
+
+	va_end(args);
+
+	return (res);
+}
