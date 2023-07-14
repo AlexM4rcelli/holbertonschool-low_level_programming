9dfb14e47ea017f809717ef3f11941bfb3c2a3a9
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-13 18:27:37 -0700

    Fixing stdarg bug

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef _MAIN_
 #define _MAIN_
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+typedef struct types_prints
+{
+	char *type;
+	void (*func)(va_list args);
+} func_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
