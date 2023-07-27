cd3d8a3d6394a2db92ab568e7a6b19dbc84d4ada
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-27 07:18:38 -0700

    Adding print_list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,7 +20,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_list(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
 size_t list_len(const dlistint_t *h);
 dlistint_t *add_node(dlistint_t **head, const char *str);
 dlistint_t *add_node_end(dlistint_t **head, const char *str);
