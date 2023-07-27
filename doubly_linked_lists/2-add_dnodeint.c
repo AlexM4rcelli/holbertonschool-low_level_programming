7571f70d1d7bfa46cca4f0fe8acea0c93671329c
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-27 07:50:05 -0700

    adding exercise 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,19 @@
+# include "lists.h"
+
+dlistint_t
+*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->prev = *head;
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
