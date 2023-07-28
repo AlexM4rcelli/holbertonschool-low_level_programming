31d9e230b50c6f9b58c6eb0e7a5d6e556fc1bb01
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-28 09:06:21 -0700

    Adding exercise 3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,28 @@
+# include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *curr;
+
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = *head;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	curr = *head;
+	while (curr->next)
+		curr = curr->next;
+
+	curr->next = new;
+
+	return (new);
+}
