d2b103f290e976da5e480ddee7ea0a84d4b30bd5
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-29 20:32:18 -0700

    #7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,17 +9,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new)
 		return  (NULL);
 
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = curr;
+		if (curr)
+			curr->prev = new;
+		*h = new;
+
+		return (new);
+	}
+
 	while (curr)
 	{
 		if (index == idx)
 		{
 			new->prev = curr->prev;
-			new->n = n;
-			new->next = curr->next;
+			new->next = curr;
+
+			curr->prev = new;
+
+			return (new);
 		}
 		curr = curr->next;
 		index++;
 	}
 
+	free(new);
 	return (NULL);
 }
