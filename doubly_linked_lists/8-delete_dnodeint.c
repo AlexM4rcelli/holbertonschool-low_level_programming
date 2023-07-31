67b8da7fa52691c030b3a7eef93aeab0b60506ab
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-30 18:37:50 -0700

    Adding exercise 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+# include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	unsigned int i = 0;
+
+	if (!head && !*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (current->next)
+			current->next->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current && i < index)
+	{
+		current->prev->next = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	if (current->prev)
+		current->prev->next = current->next;
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
+	return (1);
+}
