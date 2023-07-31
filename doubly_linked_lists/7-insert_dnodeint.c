53e8931dd1c1c2b98d0e70ce628b95ad3255b253
AlexM4rcelli <6780@holbertonstudents.com>
2023-07-30 18:24:57 -0700

    #7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,50 +2,48 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *curr = *h;
-	unsigned int index = 0;
+	dlistint_t *current = *h, *new_node;
+	unsigned int i = 0;
 
-	new = (dlistint_t *) malloc(sizeof(dlistint_t));
-	if (!new)
-		return  (NULL);
-	new->n = n;
-	if (idx == 0 && curr == NULL)
-	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
+	if (!h)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+
 	if (idx == 0)
 	{
-		new->prev = NULL;
-		new->next = curr;
-		curr->prev = new;
-		*h = new;
-		return (new);
+		new_node->prev = NULL;
+
+		new_node->next = *h;
+
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	while (curr)
+
+	while (current && i < idx - 1)
 	{
-		if (index == idx)
-		{
-			new->prev = curr->prev;
-			new->next = curr;
-
-			if (curr->prev)
-				curr->prev->next = new;
-
-			curr->prev = new;
-			return (new);
-		}
-		curr = curr->next;
-		index++;
+		current = current->next;
+		i++;
 	}
-	if (idx == index) {
-        new->prev = curr;
-        new->next = NULL;
-        if (curr)
-            curr->next = new;
-        return (new);
+	if (!current)
+	{
+		free(new_node);
+		return (NULL);
 	}
-	return (NULL);
+
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next)
+		current->next->prev = new_node;
+
+	current->next = new_node;
+
+    return (new_node);
 }
