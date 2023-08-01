e3cef96ce5090105067218714b2de51c7d93775c
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-01 10:23:12 -0700

    Fixing betty issues

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	file_descriptor = open(filename, O_WRONLY| O_APPEND);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (file_descriptor == -1)
 		return (-1);
 
