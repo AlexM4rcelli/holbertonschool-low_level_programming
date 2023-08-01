6d605687c00e0b49ffb7a8bf6433234c96a99ff9
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-01 09:58:15 -0700

    Fixing NULL text_content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_descriptor == -1)
@@ -25,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 		wrote = write(file_descriptor, text_content, i);
 	else
-		wrote = write(file_descriptor, "", 0);
+		wrote = write(file_descriptor, text_content, 0);
 	if (wrote == -1)
 	{
 		close(file_descriptor);
