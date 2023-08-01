f0db7593c9d37aa9d472fc8af66f90c283a9de28
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-01 09:40:25 -0700

    Fix #1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,16 +1,23 @@
 # include "main.h"
 
-int
-create_file(const char *filename, char *text_content)
+/**
+ * create_file - creates a file
+ *
+ * @filename: the name of the created file
+ * @text_content: the content of the file
+ *
+ * Return: 1 if succeded, -1 if not.
+ */
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t i, readed, writed;
+	ssize_t i, readed, wrote;
 	int file_descriptor;
 	char *buff;
 
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
@@ -19,8 +26,11 @@ create_file(const char *filename, char *text_content)
 	buff = malloc(i);
 	if (!buff)
 		return (-1);
-	writed = write(STDOUT_FILENO, buff, i);
-	if (writed == -1)
+	if (text_content)
+		wrote = write(file_descriptor, buff, i);
+	else
+		wrote = write(file_descriptor, "", 0);
+	if (wrote == -1)
 	{
 		close(file_descriptor);
 		free(buff);
@@ -28,7 +38,7 @@ create_file(const char *filename, char *text_content)
 	}
 	close(file_descriptor);
 	file_descriptor = open(filename, O_RDONLY);
-	readed = read(file_descriptor, buff, writed);
+	readed = read(file_descriptor, buff, wrote);
 	if (readed == -1)
 	{
 		close(file_descriptor);
