9b9cf1cc8a45cff2d36e350782c5b87cd0e5b1d0
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-02 08:52:20 -0700

    Fixing close and read issues

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,51 +4,53 @@ void
 cp(char *file_from, char *file_to)
 {
 	ssize_t wrote, readed;
-	int file_descriptor;
+	int fd_from, fd_to;
 	char text_cont[1024];
 
-	file_descriptor = open(file_from, O_RDONLY);
+	fd_from = open(file_from, O_RDONLY);
 
-	if (file_descriptor == -1)
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	readed = read(file_descriptor, text_cont, sizeof(text_cont));
+	fd_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
+	readed = read(fd_from, text_cont, sizeof(text_cont));
 
 	if (readed == -1)
 	{
-		close(file_descriptor);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
 
-	if (close(file_descriptor) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_descriptor);
-		exit(100);
-	}
-
-	file_descriptor = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
-
-	if (file_descriptor == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	wrote = write(file_descriptor, text_cont, readed);
+	wrote = write(fd_to, text_cont, readed);
 	if (wrote == -1 || wrote != readed)
 	{
-		close(file_descriptor);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		close(fd_to);
 		exit(99);
 	}
 
-	if (close(file_descriptor) == -1)
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 }
