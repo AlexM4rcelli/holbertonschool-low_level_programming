a5f4b2e138fb7734bbb8162486d5f85c305918ce
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-03 09:58:10 -0700

    Adding exercise 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,30 +1,42 @@
 # include "hash_tables.h"
 
+hash_node_t
+*add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (!new)
+		return (NULL);
+
+	(new)->key = malloc(strlen(key) + 1);
+	(new)->value = malloc(strlen(value) + 1);
+
+	strcpy(new->key, key);
+	strcpy(new->value, value);
+	(new)->next = *head;
+
+	*head = new;
+
+	return (new);
+}
+
 int
 hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *curr;
-	int idx = hash_djb2(key);
+	hash_node_t *new_node = add_node((ht)->array, key, value);
+	int idx = key_index((const unsigned char *)key, ht->size);
 
 	if (!ht)
 		return (0);
 
-	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
-	(new_node)->key = malloc(strlen(key) + 1);
-	(new_node)->value = malloc(strlen(value) + 1);
-
 	if (!(new_node)->key || !(new_node)->value)
 		return (0);
-
-	strcpy(new_node->key, key);
-	strcpy(new_node->value, value);
-	curr = (ht)->array[idx];
-
-	if (!curr)
+	if (!(ht)->array[idx])
 		(ht)->array[idx] = new_node;
 	else
 	{
-		if (strcmp((curr)->key, key))
+		if (strcmp((ht)->array[idx]->key, key))
 		{
 			strcpy((ht)->array[idx]->value, value);
 		}
