aed5cea9c31607f4f96bc1976038720dbfee5f83
AlexM4rcelli <6780@holbertonstudents.com>
2023-08-03 19:02:36 -0700

    Fixing seg fault

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,15 +3,27 @@
 char
 *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	int idx = key_index((unsigned const char *)key, (ht)->size);
+	int idx;
+	hash_node_t *curr;
 
 	if (!ht || (strlen(key)) == 0)
 		return (NULL);
 
-	if ((ht)->array[idx])
+	idx = key_index((unsigned const char *)key, (ht)->size);
+	curr = (ht)->array[idx];
+
+	if (curr)
 	{
-		if ((strcmp((ht)->array[idx]->key, key) == 0))
-			return ((ht)->array[idx]->value);
+		if (curr->next)
+		{
+			while (curr->next)
+			{
+				if ((strcmp(curr->key, key)) == 0)
+					return (curr->value);
+				curr = curr->next;
+			}
+		} else if ((strcmp(curr->key, key) == 0))
+			return (curr->value);
 	}
 
 	return (NULL);
